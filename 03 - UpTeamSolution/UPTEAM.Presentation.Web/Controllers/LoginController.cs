c523a074e566b5ba86c97c01438351cfa44e7955
Vinícius Campos <vpcampos@outlook.com>
2017-04-25 14:05:55 -0300

    Estruturação das Views no MVC

---
+++ 03 - UpTeamSolution/UPTEAM.Presentation.Web/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace UPTEAM.Presentation.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ 03 - UpTeamSolution/UPTEAM.Presentation.Web/Controllers/MensagemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UPTEAM.Presentation.Web.Controllers
+{
+    public class MensagemController : Controller
+    {
+        // GET: Mensagem
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Criar()
+        {
+            return View();
+        }
+        public ActionResult Ler()
+        {
+            return View();
+        }
+    }
+}
