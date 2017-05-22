3646dedc5ea41ccf3518ad9fc40468250a5445f2
Vinícius Campos <vpcampos@outlook.com>
2017-05-22 15:21:45 -0300

    Criação de Services

---
+++ UPTEAM.Infra.Data/Repositories/ProjetoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UPTEAM.Domain.Entities;
+using UPTEAM.Domain.RepositoryInterfaces;
+
+namespace UPTEAM.Infra.Data.Repositories
+{
+    public class ProjetoRepository : RepositoryBase<tb_projeto>, IProjetoRepository
+    {
+        public IEnumerable<tb_projeto> GetProjectByTeam(int equipeId)
+        {
+            return Db.Set<tb_projeto>().Where(x => x.idt_equipe == equipeId);
+        }
+    }
+}
