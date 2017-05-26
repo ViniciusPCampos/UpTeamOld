e6ebdfcc783e0e586c93a200695f592ed39172e5
Vinícius Campos <vpcampos@outlook.com>
2017-05-26 08:31:08 -0300

    Criação do Registrar Ususario

---
+++ UPTEAM.AutoMapper/Parses/RegistrarModelToTbUsuarioParse.cs
@@ -0,0 +1,10 @@
+﻿using UPTEAM.AutoMapper.Parses.Interfaces;
+using UPTEAM.Domain.Entities;
+using UPTEAM.Models;
+
+namespace UPTEAM.AutoMapper.Parses
+{
+    public class RegistrarModelToTbUsuarioParse : ParseBase<RegistrarModel,tb_usuario>, IRegistrarModelToTbUsuarioParse
+    {
+    }
+}
