13eb24a4f6b0b858457c39e62247e82b6f45f6e4
Vinícius Campos <vpcampos@outlook.com>
2017-06-19 13:02:36 -0300

    Criação de Create Projeto

--- UPTEAM.Models/ProjetoModel.cs
+++ UPTEAM.Models/ProjetoModel.cs
@@ -4,6 +4,7 @@ namespace UPTEAM.Models
 {
     public class ProjetoModel
     {
+        public int ID { get; set; }
         public string NomeProjeto { get; set; }
         public string DescricaoProjeto { get; set; }
         public DateTime DataInicioProjeto { get; set; }
