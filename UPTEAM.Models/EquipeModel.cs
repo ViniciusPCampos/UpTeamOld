de2fa0c4e3b8285ce9b672c383abec9d80396996
Vinícius Campos <vpcampos@outlook.com>
2017-06-29 20:28:00 -0300

    Fix 1.1

--- UPTEAM.Models/EquipeModel.cs
+++ UPTEAM.Models/EquipeModel.cs
@@ -5,7 +5,7 @@ namespace UPTEAM.Models
 {
     public class EquipeModel
     {
-        public int Id { get; set; }
+        public int IdEquipe { get; set; }
         [Required]
         public string NomeEquipe { get; set; }
         [Required]
