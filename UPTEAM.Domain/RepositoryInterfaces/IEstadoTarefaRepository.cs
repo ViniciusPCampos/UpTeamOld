d57a85d51f2fca07482e74c5be66073b7a1efd91
Lucas Prates <pratesdtr@gmail.com>
2017-06-30 16:16:04 -0300

    Repositorios Restantes

---
+++ UPTEAM.Domain/RepositoryInterfaces/IEstadoTarefaRepository.cs
@@ -0,0 +1,9 @@
+﻿using UPTEAM.Domain.Entities;
+
+namespace UPTEAM.Domain.RepositoryInterfaces
+{
+    public interface IEstadoTarefaRepository : IRepositoryBase<tt_estado_tarefa>
+    {
+
+    }
+}
---
+++ UPTEAM.Infra.Data/Repositories/TipoTarefaRepository.cs
@@ -0,0 +1,10 @@
+﻿using UPTEAM.Domain.Entities;
+using UPTEAM.Domain.RepositoryInterfaces;
+
+namespace UPTEAM.Infra.Data.Repositories
+{
+    public class TipoTarefaRepository : RepositoryBase<tt_tipo_tarefa>, ITipoTarefaRepository
+    {
+
+    }
+}
